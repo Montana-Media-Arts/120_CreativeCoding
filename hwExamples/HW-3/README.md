deaab2e08a6f44670b624e0f2fc208e3ba8a0e1b
Michael Musick <michael@michaelmusick.com>
2017-02-08 09:49:21 -0700

    self-portrait HW-3 example

---
+++ hwExamples/HW-3/README.md
@@ -0,0 +1,13 @@
+# HW-3 -- Self Portrait
+
+This week I finally dug into p5.js and made a Self-Portrait (reinterpreted as a monster).
+
+etc.............
+
+
+## Final Picture
+
+My sketch is available at my [GitHub site]().
+
+Here is a screen shot of my sketch.
+![Screen shot of my sketch](imgs/selfportrait.png "Screen shot of my monster sketch")
---
+++ hwExamples/HW-3/sketch.js
@@ -0,0 +1,79 @@
+function setup() {
+    // Setup basic elements
+    createCanvas(500, 500);
+    background(255, 247, 240);
+
+    // Main body
+    push();
+    translate(250, 250);
+    rectMode(CENTER);
+    fill(200, 10, 20);
+    rect(0, 0, 200, 300);
+    line(15, -150, 15, 150);
+    fill(0);
+    ellipse(-5, -50, 15, 15);
+    ellipse(-5, -10, 15, 15);
+    ellipse(-5, 30, 15, 15);
+    ellipse(-5, 70, 15, 15);
+    ellipse(-5, 110, 15, 15);
+    pop();
+
+    // Legs
+    push();
+    translate(250, 400);
+    fill(90, 50, 245);
+    triangle(10, 0, 100, 0, 210, 190);
+    triangle(-10, 0, -100, 0, -210, 190);
+    pop();
+
+    // Arms
+    push();
+    translate(250, 200);
+    strokeWeight(20);
+    line(80, 0, 140, 0);
+    line(140, 0, 210, -50);
+    fill(0, 0, 0);
+    ellipse(210, -50, 20, 20);
+    line(-80, 0, -140, 40);
+    line(-140, 40, -190, 150);
+    fill(0, 0, 0);
+    ellipse(-190, 150, 20, 20);
+    pop();
+
+    push();
+    // Head
+    translate(250, 100);
+    fill(200, 10, 200);
+    ellipse(0, 0, 400, 125);
+
+    // eyes
+    fill(250, 249, 255);
+    ellipse(-80, -20, 100, 40);
+    fill(0, 0, 0);
+    ellipse(-80, -20, 40, 40);
+
+    fill(250, 249, 255);
+    ellipse( 80, -20, 100, 10);
+    fill(0, 0, 0);
+    ellipse( 80, -20, 60, 5);
+
+    // mouth
+    fill(2, 240, 250);
+    arc(0, 30, 200, 60, 0, PI, CHORD);
+
+    // nose
+    fill(200, 90, 170);
+    triangle(-10, -40, -10, 15, 70, 80);
+
+    // end of head
+    pop();
+
+    // Naming
+    push();
+    textAlign(CENTER);
+    textSize(20);
+    textFont("Georgia");
+    text("A Self-Portrait, by Michael Musick", 250, 27);
+    pop();
+
+}
