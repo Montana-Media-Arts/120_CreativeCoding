6a0e23f5f5c3437646dffd42038b75ff503070c7
Michael Musick <michael@michaelmusick.com>
2017-04-03 09:36:27 -0600

    changes from class

--- inClass/09-1/02-classes/happyface.js
+++ inClass/09-1/02-classes/happyface.js
@@ -4,37 +4,42 @@
 // Constructor Method for the class 'Happyface'
 // A constructor method is used to create a new object
 // NOTE: Typically you should capitalize class names
-function Happyface(init_x, init_y, init_size, delta){
+function Happyface(init_x, init_y, init_size, delta, fillColor) {
     this.x = init_x;
     this.y = init_y;
     this.size = init_size;
     this.delta = delta;
-
-    this.move = function(){
-        this.x += this.delta;
-        this.checkEdges();
-    };
-
-    this.checkEdges = function(){
-        if (this.x >= width) {
-            this.x = width - 1;
-            this.delta *= -1;
-        } else if (this.x <= 0) {
-            this.x = 0 + 1;
-            this.delta *= -1;
-        }
-    }
+    this.fillColor = fillColor;
 }
+
+Happyface.prototype.move = function() {
+    // this.x += this.delta;
+    this.x = this.x + this.delta;
+    this.checkEdges();
+};
+
+Happyface.prototype.checkEdges = function() {
+    if (this.x >= width) {
+        this.x = width - 1;
+        this.delta *= -1;
+    } else if (this.x <= 0) {
+        this.x = 0 + 1;
+        this.delta *= -1;
+    }
+};
+
 
 // Another method to be called
 // This will display our happyface object.
-Happyface.prototype.display = function(){
+Happyface.prototype.display = function() {
     push();
     translate(this.x, this.y);
 
+    fill(this.fillColor);
     ellipse(0, 0, this.size, this.size);
 
-    ellipse(-this.size*0.25, -this.size*0.25, this.size*0.2, this.size*0.2);
-    ellipse(this.size*0.25, -this.size*0.25, this.size*0.2, this.size*0.2);
+    fill('white');
+    ellipse(-this.size * 0.25, -this.size * 0.25, this.size * 0.2, this.size * 0.2);
+    ellipse(this.size * 0.25, -this.size * 0.25, this.size * 0.2, this.size * 0.2);
     pop();
 };
--- inClass/09-1/02-classes/sketch.js
+++ inClass/09-1/02-classes/sketch.js
@@ -1,25 +1,45 @@
 
 var bg_color = '#d78fca';
 
-var happyfaces = [];
+var objArray = [];
 
 function setup() {
     createCanvas(800,600);
     background(bg_color);
 
-    for (var i = 0; i < 1000; i++) {
-        happyfaces.push(
-            new Happyface(random(width), random(height), random(100), random(10) )
+    for (var i = 0; i < 10; i++) {
+        objArray.push(
+            new Happyface(0, random(height), random(100), random(20), randomColor())
         );
     }
 }
 
 function draw() {
-    background(bg_color);
+    bg_render();
 
-    for (var i = 0; i < happyfaces.length; i++) {
-        happyfaces[i].display();
-        happyfaces[i].move();
+    for (var i = 0; i < objArray.length; i++) {
+        objArray[i].display();
+        objArray[i].move();
     }
 
+}
+
+
+function bg_render(){
+    background(bg_color);
+    for (var i = 20; i < width; i += 100) {
+        for (var j = 20; j < height; j+=100) {
+            fill('#b3b681');
+            rect(i,j,60,60);
+        }
+    }
+}
+
+
+function randomColor(){
+    return color(
+        floor(random(255)),
+        floor(random(255)),
+        floor(random(255))
+    );
 }
