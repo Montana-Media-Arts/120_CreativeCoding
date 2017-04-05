355a21fb7f44fda2a7a314d8129d8db23566a4e0
Michael Musick <michael@michaelmusick.com>
2017-04-05 16:13:35 -0600

    bouncing balls

---
+++ inClass/10-2/00-bouncing-balls/sketch.js
@@ -0,0 +1,33 @@
+
+var balls = [];
+var num_of_balls = 5;
+var bg_color = '#323338';
+
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+
+    for (var i = 0; i < num_of_balls; i++) {
+        balls.push( new BouncingBall(
+            random(width),
+            random(height),
+            random(40, 80),
+            randColor()
+        ));
+    }
+}
+
+function draw() {
+    background(bg_color);
+
+
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].display();
+        balls[i].move();
+        balls[i].collisionCheck(balls, i);
+    }
+}
+
+
+function randColor(){
+    return color( floor(random(255)), floor(random(255)), floor(random(255)) );
+}
---
+++ inClass/10-2/readme.md
@@ -0,0 +1,1 @@
+Review how to make objects interact. 
