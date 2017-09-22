6f2b6f3b85b3ff33058f094179e220366f9e104a
Michael Musick <michael@michaelmusick.com>
2017-09-22 13:05:29 -0600

    changed background example

--- lecture_code/03/03_background_01/sketch.js
+++
@@ -1,5 +0,0 @@
-function setup() {
-    // notice there is no canvas command in the sketch file
-    // blue background to demonstrate default canvas size
-    background('blue');
-}
---
+++ lecture_code/03/04_background_01/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    // first we need to specify our canvas size
+    createCanvas(800,400);
+
+    // Then we can color this background
+    // Notice how the word 'blue' is surrounded in quotes
+    background('blue');
+    // Try replaceing the word 'blue' with other common color names.
+    // NOTE: Be sure to keep teh word surrounded with quotes.
+}
