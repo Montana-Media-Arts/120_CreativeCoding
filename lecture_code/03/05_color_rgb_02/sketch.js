3d6a46d9ca887bbc4dc3ae4186bfcf34c2325891
Michael Musick <michael@michaelmusick.com>
2017-09-22 13:49:10 -0600

    hex example

--- lecture_code/03/05_color_rgb_02/sketch.js
+++ lecture_code/03/05_color_rgb_02/sketch.js
@@ -2,6 +2,8 @@ function setup() {
     // first we need to specify our canvas size
     createCanvas( 800, 400 );
 
-    // Try replacing one of the integer value parameters to the background() function.
-    background( 255, 123, 2 );
+    // Try changing any of the characters after the `#`
+    // Remember to keep them values between 0-f
+    // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, a, b, c, d, e, f].
+    background( '#ff00ff' );
 }
