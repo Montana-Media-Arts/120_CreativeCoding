4f3978e211d71bba9e041914d172315d466e8cd2
Michael Musick <michael@michaelmusick.com>
2017-09-22 11:12:52 -0600

    create canvas example added

--- lecture_code/03/03_background_01/sketch.js
+++ lecture_code/03/03_background_01/sketch.js
@@ -1,3 +1,8 @@
-function setup() {
-    background('blue');
+function setup(){
+    // Change the number in the createCanvas() function
+    createCanvas( 300, 100 );
+
+    // Ignore this line for the moment,
+    // This is what is "coloring" the canvas
+    background( 'blue' );
 }
