894199a895e57250f0f780e2086b9578bc995c1b
Michael Musick <michael@michaelmusick.com>
2017-09-22 13:30:15 -0600

    rgb example 1

---
+++ lecture_code/03/05_color_rgb_01/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    // first we need to specify our canvas size
+    createCanvas( 800, 400 );
+
+    // Try replacing one of the integer values in the above RGB color function.
+    // NOTE: Be sure to keep the rgb() function surrounded with quotes.
+    background( 'rgb(255, 123, 2)' );
+}
