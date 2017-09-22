a920e4bedce127e32f2fe33095e58cdd40455869
Michael Musick <michael@michaelmusick.com>
2017-09-22 16:57:35 -0600

    four croner point example

---
+++ lecture_code/03/06_point_02/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas( 300, 100 );
+    background( '#c8f1f9' );
+}
+
+function draw() {
+    point(30, 20);
+    point(85, 20);
+    point(85, 75);
+    point(30, 75);
+}
