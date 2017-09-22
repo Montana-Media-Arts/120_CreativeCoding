a6128a8a6dbd11576178775d5fd5739bb42bfac0
Michael Musick <michael@michaelmusick.com>
2017-09-22 11:53:58 -0600

    der

--- lecture_code/03/03_createCanvas_01/sketch.js
+++ lecture_code/03/03_createCanvas_01/sketch.js
@@ -1,6 +1,6 @@
 function setup(){
     // Change the number in the createCanvas() function
-    createCanvas( 600, 400 );
+    createCanvas( 600, 4000 );
 
     // Ignore this line for the moment,
     // This is what is "coloring" the canvas
