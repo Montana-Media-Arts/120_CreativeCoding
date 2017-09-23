ded6acad5ae500ef7345c1314c1534fc17f707ed
Michael Musick <michael@michaelmusick.com>
2017-09-22 18:21:18 -0600

    rect_03

---
+++ lecture_code/03/07_rect_03/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas( 500, 400 );
+    background( '#e3f9c8' );
+}
+
+function draw() {
+    // a rectangle with various corner angles
+    rect( 20, 20, 300, 300, 0, 90, 45, 130 );
+}
