57f2997b994cbd01b73d358ffc9206b7546a15c4
Michael Musick <michael@michaelmusick.com>
2017-09-28 10:31:13 -0600

    changes to smiley scale

--- lecture_code/04/13_scaleSmiley_02/sketch.js
+++ lecture_code/04/13_scaleSmiley_02/sketch.js
@@ -1,18 +1,14 @@
 function setup(){
     // create a canvas that is
     // (width:400px, height:600px)
-    createCanvas(400,400);
+    createCanvas(300,400);
 }
 
 function draw() {
     // set the background to 'white'
     background(255);
 
-
-    // scale the x-axis by 200%
-    // scale the y-axis by 75%
-    scale(2.0, 0.75);
-    translate( 75, 75 );
+    translate( 150, 75 );
     // ** draw the smily face **
     stroke( 0 );
     fill('rgba(234, 255, 61, 1.0)');
@@ -23,4 +19,17 @@ function draw() {
     ellipse( -20, -15, 20 );
     ellipse( 20, -15, 20 );
 
+    // scale the x-axis by 200%
+    // scale the y-axis by 75%
+    translate( 0, 125 );
+    scale(0.25, 3.0);
+    // ** draw the smily face **
+    stroke( 0 );
+    fill('rgba(234, 255, 61, 1.0)');
+    ellipse( 0, 0, 100 );
+    noStroke();
+    fill( 40, 255 );
+    arc( 0, 15, 75, 50, 0, PI );
+    ellipse( -20, -15, 20 );
+    ellipse( 20, -15, 20 );
 }
