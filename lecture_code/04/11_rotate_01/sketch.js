3873e113f6b3f3f2c9bacb7e0eda0ab3fc39e521
Michael Musick <michael@michaelmusick.com>
2017-09-28 09:57:44 -0600

    rotate example

---
+++ lecture_code/04/11_rotate_01/sketch.js
@@ -0,0 +1,19 @@
+function setup(){
+    createCanvas(400,400);
+}
+
+function draw() {
+    background(255);
+
+    // move center grid point (x:0, y:0)
+    // to the center of the canvas
+    translate( 200, 200 );
+
+    // rotate the grid by 15°
+    rotate( radians(15) );
+
+    // draw a rectangle at (x:50, y:50)
+    // in relation to the grid transformations
+    fill( 'rgba(5, 64, 214, 0.8)' );
+    rect( 0, 0, 50, 50 );
+}
