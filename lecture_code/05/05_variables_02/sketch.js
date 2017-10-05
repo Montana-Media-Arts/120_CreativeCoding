5b12ff2dc79b7eda35ccd774ae0860fabffedcef
Michael Musick <michael@michaelmusick.com>
2017-10-05 06:37:17 -0600

    more fun with variables

---
+++ lecture_code/05/05_variables_02/sketch.js
@@ -0,0 +1,46 @@
+/** EXPLORING VARIABLES
+*
+* 	Sketch from p5-Examples-Variables
+* 	https://p5js.org/examples/data-variables.html
+*
+* 	TODO: Change the values in lines 13, 14, & 15
+*/
+
+// variables defining lines
+// change these values
+// (keep them as Numbers)
+// notice the changes
+var a = 50;
+var b = 120;
+var c = 180;
+
+function setup() {
+  createCanvas(720, 400);
+  background(0);
+  stroke(153);
+  strokeWeight(4);
+  strokeCap(SQUARE);
+}
+
+function draw() {
+  line(a, b, a+c, b);
+  line(a, b+10, a+c, b+10);
+  line(a, b+20, a+c, b+20);
+  line(a, b+30, a+c, b+30);
+
+  a = a + c;
+  b = height-b;
+
+  line(a, b, a+c, b);
+  line(a, b+10, a+c, b+10);
+  line(a, b+20, a+c, b+20);
+  line(a, b+30, a+c, b+30);
+
+  a = a + c;
+  b = height-b;
+
+  line(a, b, a+c, b);
+  line(a, b+10, a+c, b+10);
+  line(a, b+20, a+c, b+20);
+  line(a, b+30, a+c, b+30);
+}
