967c308df5d57c26e33ed7fb9eaacef1b6b84f39
Michael Musick <michael@michaelmusick.com>
2017-10-05 06:27:10 -0600

    variables

--- lecture_code/05/05_variables_01/sketch.js
+++ lecture_code/05/05_variables_01/sketch.js
@@ -1,23 +1,20 @@
+/* VARIABLES */
 
-// A Variable
-// -> To create we use the keyword 'var'
+// -> To declare we use the keyword 'var'
 // This declares to the computer a new piece of reserved memory
 // This memory is referenced by the namespace "myFirstVar"
 var myFirstVar;
 
 // Assign a value to the Variable
-// Said "myFirstVar *gets* 60"
+// Said "myFirstVar *gets* 120"
 myFirstVar = 120;
 
 function setup() {
-	createCanvas(800,400);
-	background(200);
-
-	translate(400,200);
-	ellipse(0,0,myFirstVar,myFirstVar);
-
+	createCanvas( 800, 400 );
+	background( 200 );
 }
 
 function draw() {
-
+	translate( 400, 200 );
+	ellipse( 0, 0, myFirstVar );
 }
