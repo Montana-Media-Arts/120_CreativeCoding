81a4953ea6b05dddb74d7173fc16c072e1482f43
Michael Musick <michael@michaelmusick.com>
2017-10-06 09:20:55 -0600

    frame rate

---
+++ lecture_code/05/11_frameRate_01/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas( 400, 400 );
+}
+
+angle = 0;
+function draw() {
+    background( 'rgb(47, 252, 68)' );
+
+    translate( width*0.5, height*0.5 );
+    rotate( radians(angle) );
+    ellipse( 0, 0, 300, 50 );
+
+    // update angle position
+    angle = angle + 45;
+}
---
+++ lecture_code/05/11_frameRate_02/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas( 400, 400 );
+
+    // set the frame rate for the sketch
+    frameRate( 2 );
+}
+
+angle = 0;
+function draw() {
+    background( 'rgb(47, 252, 68)' );
+
+    translate( width*0.5, height*0.5 );
+    rotate( radians(angle) );
+    ellipse( 0, 0, 300, 50 );
+
+    // update angle position
+    angle = angle + 45;
+}
