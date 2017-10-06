9331d6f25ee23d5188d96989f47018689b576917
Michael Musick <michael@michaelmusick.com>
2017-10-05 19:09:35 -0600

    added basic animation

--- lecture_code/05/09_basicAnimation_01/sketch.js
+++ lecture_code/05/09_basicAnimation_01/sketch.js
@@ -1,21 +1,20 @@
 function setup() {
     createCanvas(600,700);
     background(200, 40, 6);
-
 }
 
-
 // Variables to define ellipse position
+// NOTE: This *must* be defined globally.
+// Otherwise, if they were defined locally inside draw
+// they would get reset to '100' every frame,
+// and the object would not move
 var posX = 100;
 var posY = 100;
 
 function draw() {
-
-
     ellipse(posX,posY,30,30);
 
-    // update posX every frame
+    // update posX & posY every frame
     posX = posX + 1;
     posY = posY + 1;
-
 }
