1977f99d9a0c86d71631888a6bdba59d3b330871
Michael Musick <michael@michaelmusick.com>
2017-10-12 17:13:25 -0600

    random

---
+++ lecture_code/06/09_random_01/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas( windowWidth, 200 );
+    frameRate(2);
+}
+
+
+function draw() {
+    background( 'rgb(74, 52, 103)' );
+
+    // get a new random number every frame
+    var rand = random();
+
+    // print the number to the canvas
+    textSize(26);
+    noStroke();
+    fill(255);
+    text( rand, 10, height/2 );
+}
