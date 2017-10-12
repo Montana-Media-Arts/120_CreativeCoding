4b767773d4c25015a7cdeceb83ada99372e2944d
Michael Musick <michael@michaelmusick.com>
2017-10-12 08:23:22 -0600

    modulo ellipse

---
+++ lecture_code/06/02_modulo_03/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+    createCanvas( windowWidth, windowHeight );
+}
+
+// declare a variable, 'pos_x'
+// instantiate the variable to '20'
+// This will represent the position of an ellipse
+var pos_x = 20;
+
+function draw() {
+    background( 'rgb(218, 63, 161)' );
+
+    // draw an ellipse
+    fill(255);
+    noStroke();
+    ellipse( pos_x, 140, 30 );
+
+    // update the ellipses position
+    pos_x = pos_x + 5;
+    // bound it to the width of the canvas
+    pos_x = pos_x % width;
+
+    // display the equation and result
+    textSize(24)
+    text( "width: " + width, 20, 40 );
+    text( "pos_x: " + pos_x, 20, 80 );
+}
