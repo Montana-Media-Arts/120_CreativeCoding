3f694e95d83b08d598ea093e6e78294cbcada11f
Michael Musick <michael@michaelmusick.com>
2017-10-19 16:03:24 -0600

    logic or

---
+++ lecture_code/07/04_logical_not_01/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+    createCanvas( windowWidth, 300 );
+    background('black');
+
+    // adjust text display
+    fill('white');
+    textSize(36);
+    textAlign(CENTER,CENTER);
+
+    var textStr;
+
+    // pass in "NOT" true
+    if( !true ) {
+        // this block DOES execute
+        textStr = "The result was TRUE";
+    } else {
+        // THIS BLOCK DOES EXECUTE
+        textStr = "The result was TRUE";
+    }
+
+    // display the results of the if statement
+    text( textStr, width/2, height/2 );
+}
