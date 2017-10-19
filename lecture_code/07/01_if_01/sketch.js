b6a71c5c6af646768a41d05770380fc184ecd62c
Michael Musick <michael@michaelmusick.com>
2017-10-19 14:34:50 -0600

    if examples

---
+++ lecture_code/07/01_if_01/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas( windowWidth, windowHeight );
+}
+
+function draw() {
+    background('white');
+
+    // set the fill color for an ellipse
+    if( true ) {
+        fill('red');
+    } else {
+        fill('blue');
+    }
+
+    ellipse( width/2, height/2, 40 );
+}
---
+++ lecture_code/07/02_if_vars_01/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas( windowWidth, windowHeight );
+    // set a low frame rate so that you can see the change
+    // frameRate is set to 0.5 frames per second
+    // or 1 frame every 2 seconds.
+    frameRate(0.5);
+}
+
+var conditionalValue = false;
+function draw() {
+    background('white');
+
+    // set the fill color for an ellipse
+    if( conditionalValue ) {
+        fill('red');
+    } else {
+        fill('blue');
+    }
+
+    ellipse( width/2, height/2, 40 );
+
+
+    // update the conditionalValue variable
+    conditionalValue = true;
+}
