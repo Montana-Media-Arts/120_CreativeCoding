fdb7d98a8e5f67e5af18ff3e14e20b9be6fa77ad
Michael Musick <michael@michaelmusick.com>
2017-10-19 18:32:30 -0600

    inequality

---
+++ lecture_code/07/06_inequality_01/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+    createCanvas( windowWidth, 300 );
+    // adjust text display
+    textSize(36);
+    textAlign(CENTER,CENTER);
+}
+
+var searchNum = -10000;
+
+function draw() {
+
+    if( searchNum != 0 ) {
+        background( random(255), random(255), random(255) );
+        text("searching for 0...", width/2, height/2);
+
+        // get a new number to try next time
+        searchNum = floor(random(200));
+    } else {
+        background( 'pink' );
+        // after finding a random 0, celebrate
+        text("Found a random 0!!!", width/2, height/2);
+    }
+}
