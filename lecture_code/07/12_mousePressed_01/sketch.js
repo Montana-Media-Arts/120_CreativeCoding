1ccad3095771e55bc59c745f69c11379785f0d43
Michael Musick <michael@michaelmusick.com>
2017-10-19 20:49:27 -0600

    mousePressed

---
+++ lecture_code/07/12_mousePressed_01/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas( windowWidth, 400 );
+}
+
+var mouseCounter = 0;
+
+function draw() {
+    background( 'rgb(171, 93, 193)' );
+
+    textSize(24);
+    textAlign(CENTER);
+    fill(255);
+
+    // print the number of times
+    // the mouse has been pressed.
+    var str = "Mouse clicked " + mouseCounter + " times.";
+    text( str, width/2, height/2 );
+}
+
+// mouse pressed event function
+function mousePressed() {
+    mouseCounter++;
+    return false;
+}
