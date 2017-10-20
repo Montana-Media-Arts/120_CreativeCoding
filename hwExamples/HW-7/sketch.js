1b599f41a451e6f739cd1c32a1fd6b9b0a1b24a6
Michael Musick <michael@michaelmusick.com>
2017-10-19 23:16:11 -0600

    bouncing ball for HW

--- hwExamples/HW-7/sketch.js
+++ hwExamples/HW-7/sketch.js
@@ -1,4 +1,5 @@
-// create a ball object to store values in
+
+
 var ball = {};
 ball.width = 40;
 ball.x = 10;
@@ -14,16 +15,13 @@ function setup() {
 }
 
 
-// MOVE THE BALL AND KEEP THE BALL ON THE SCREEN.
+
 function draw() {
-    // update the ball X and Y position
-    // a "delta" or change per frame value
-    // is added to the X and Y position
+
     ball.x += ball.delta_x * ball.scale_x;
     ball.y += ball.delta_y * ball.scale_y;
 
-    // if the ball is beyond the canvas edge,
-    // change direction
+
     if (ball.x >= width || ball.x <= 0) {
         ball.delta_x = -1 * ball.delta_x;
     }
@@ -31,13 +29,11 @@ function draw() {
         ball.delta_y = -1 * ball.delta_y;
     }
 
-    // draw the ball
-    ellipse(ball.x, ball.y, ball.width, ball.width);
 
+    ellipse(ball.x, ball.y, ball.width, ball.width);
 }
 
 function mousePressed() {
-    // update delta_values based on mouse position
     ball.scale_x = map(mouseX, 0, width, 0.5, 10);
     ball.scale_y = map(mouseY, 0, height, 0.5, 10);
 }
